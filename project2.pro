c3dd4b0986c0999365f2f018afacd8136909b5dc
moha255 <m.hassan255@yahoo.com>
2015-10-21 23:42:40 +0200

    added the first file

---
+++ project2.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-10-21T23:19:25
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = project2
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS += \
+    stock20.h
---
+++ stock20.h
@@ -0,0 +1,27 @@
+#ifndef STOCK20_H
+#define STOCK20_H
+#include <string>
+class Stock
+{
+private:
+std::string company;
+int shares;
+double share_val;
+double total_val;
+void set_tot() { total_val = shares * share_val; }
+public:
+Stock();        // default constructor
+Stock(const std::string & co, long n = 0, double pr = 0.0);
+~Stock();       // do-nothing destructor
+void buy(long num, double price);
+void sell(long num, double price);
+void update(double price);
+void show()const;
+const Stock & topval(const Stock & s) const;
+// new added members
+const int & get_shares();
+const double & get_share_val();
+const double & get_total_val();
+
+};
+#endif // STOCK20_H
